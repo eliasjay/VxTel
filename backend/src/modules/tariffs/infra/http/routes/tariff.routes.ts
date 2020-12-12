ebb726018b86644d704d90ec1f612f32527262e4
eliasjay <zeliasgl@gmail.com>
2020-12-12 20:59:23 -0300

    routes

--- backend/src/modules/tariffs/infra/http/routes/tariff.routes.ts
+++ backend/src/modules/tariffs/infra/http/routes/tariff.routes.ts
@@ -4,8 +4,8 @@ import TariffController from "../controllers/TariffController";
 
 const tariffController = new TariffController()
 
-const tariffRouter = Router()
+const tariffsRouter = Router()
 
-tariffRouter.post('/', tariffController.index)
+tariffsRouter.post('/', tariffController.index)
 
-export default tariffRouter
+export default tariffsRouter
--- backend/src/shared/routes/index.ts
+++ backend/src/shared/routes/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
 
+import tariffsRouter from "@modules/tariffs/infra/http/routes/tariff.routes";
+import plansRouter from "@modules/plans/infra/http/routes/plans.routes";
+
 const routes = Router()
+
+routes.use('/tariffs', tariffsRouter)
+routes.use('/plans', plansRouter)
 
 export default routes
