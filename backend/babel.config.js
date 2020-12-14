da92e3753aa21e2f9a005041f5870a1515424af5
eliasjay <zeliasgl@gmail.com>
2020-12-13 22:33:36 -0300

    fix build

--- backend/babel.config.js
+++ backend/babel.config.js
@@ -11,12 +11,9 @@ module.exports = {
     '@babel/preset-typescript'
   ],
   plugins: [
-    [
-      "@babel/plugin-proposal-decorators",
-      {
-        "legacy": true
-      }
-    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
     ['module-resolver', {
       alias: {
         '@shared': './src/shared',
